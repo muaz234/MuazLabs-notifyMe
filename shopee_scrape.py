f5a1f9d326c48051f1bbdc321bd175c63f20143c
muaz234 <ahmedmuaz0152@gmail.com>
2021-07-06 07:45:12 +0800

    improve code readibility

--- shopee_scrape.py
+++ shopee_scrape.py
@@ -6,28 +6,21 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import *
-
 # /usr/bin/python
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
-
-
 url = 'https://shopee.com.my/product/66318056/7658696983/'
 # url = 'https://shopee.com.my/product/724335/2212828002' // sample for testing purpose
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 driver.get(url)
-
 # click the english language button
 driver.find_element_by_xpath("""//*[@id="modal"]/div[1]/div[1]/div/div[3]/div[1]/button""").click()
 sleep(3)
 # click disc version 
 driver.find_element_by_xpath("""//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div[4]/div/div[1]/div/button[1]""").click();
-
 # stock reading count on the product detail section
 out = driver.find_element_by_xpath("""//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div[4]/div/div[2]/div[2]/div[2]""").text
 
-exit
 if out == "0 piece available":
     print("Product still out of stock")
     message = Mail(
